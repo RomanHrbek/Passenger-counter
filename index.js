ed49f2be03b941af7928bc9daa3a4a667398688a
RomanHrbek <roman.hrbek@seznam.cz>
2023-09-26 23:55:27 +0200

    Counter app finished

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
-console.log(countEl)
 let count = 0
+
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
+
 function save() {
     let countStr = count + " - "
-    saveEl.innerText += countStr 
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
 
